cbc0e41a03ca4351fc953dde1263bde905ef6bf7
Tobias Kauer <tobias.kauer@fh-potsdam.de>
2021-06-26 17:48:07 +0200

    update server port (&build)

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     //baseURL: 'https://corona-memories-server.herokuapp.com',
-    //baseURL: 'http://coroname.uber.space:',
+    //baseURL: 'https://coroname.uber.space:',
     //baseURL: 'http://localhost:40980',
-    baseURL: 'http://localhost:3000',
-    //baseURL: 'https://coroname.uber.space',
+    //baseURL: 'http://localhost:3000',
+    baseURL: 'https://coroname.uber.space',
   })
 }
